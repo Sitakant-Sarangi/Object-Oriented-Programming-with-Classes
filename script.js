1854c96c47d2733b3ae73a4fbb67d40b62dbdde5
Sitakant Sarngi <sitakantsarangi1981@gmail.com>
2026-03-24 22:53:00 -0400

    updated code

--- script.js
+++ script.js
@@ -26,9 +26,19 @@ class PerishableProduct extends Product {
     return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
   }
 }
+
 const milk = new PerishableProduct("Milk", 1.50, 10, "2024-12-31");
 console.log(milk.toString());
 const butter = new PerishableProduct("Butter", 2.00, 5, "2024-12-31");
 console.log(butter.toString());
 const products = [milk, butter];
+
+
+
+static applyDiscount(products, discount) {
+        products.forEach(product => {
+            product.price = product.price * discount;
+        });
+    }
+}
 
